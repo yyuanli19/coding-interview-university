fd878a4f0244fe9128d440b25c27eb539ed39003
yyuanli19 <yyuanli19@gmail.com>
2020-12-24 16:43:39 +0100

    added found interesting solution for plus one in array

--- code/array/add_one_divide_conquer.py
+++ code/array/add_one_divide_conquer.py
@@ -15,5 +15,21 @@ class Solution:
             digits = self.plusOne(digits[:-1]) + [digits[-1]]
         return digits
 
+"""
+    found very interesting solution
+    Runtime: 28 ms, faster than 86.54% of Python3 online submissions for Plus One.
+    Memory Usage: 14 MB, less than 85.15% of Python3 online submissions for Plus One.
+"""
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)):
+            if digits[~i] < 9:
+                digits[~i] += 1
+                return digits
+            digits[~i] = 0
+        return [1] + [0] * len(digits)
+
+
+        
 
     
