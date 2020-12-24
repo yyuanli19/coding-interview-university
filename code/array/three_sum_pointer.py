4910c0bfa7e7c7d37f3252a6b7826141571e798c
yyuanli19 <yyuanli19@gmail.com>
2020-12-24 11:51:37 +0100

    added other pointer solutions found online

--- code/array/three_sum_pointer.py
+++ code/array/three_sum_pointer.py
@@ -39,6 +39,35 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
             p_large -= 1
     return anw
   
-    
+   
+"""
+some other pointer solution found in discussion
+Runtime: 860 ms, faster than 63.49% of Python3 online submissions for 3Sum.
+Memory Usage: 16.8 MB, less than 97.73% of Python3 online submissions for 3Sum.
+
+"""
+
+class Solution2:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
+        return res
 
     
